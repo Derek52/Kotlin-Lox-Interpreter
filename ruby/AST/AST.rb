75fac124a83685ef5c752757d548c1d5ff39f15e
Derek Briggs <contact@derekbriggs.xyz>
2022-12-16 22:21:52 -0500

    Amend Ruby script to write Expr.kt

--- ruby/AST/AST.rb
+++ ruby/AST/AST.rb
@@ -1,7 +1,20 @@
 $basename = "Expr"
 
 def defineType(file, className, fieldList)
-  file.write("class #{className}(#{fieldList}) : #{$basename}() {\n}\n\n")
+  file.write("class #{className}(#{fieldList}) : #{$basename}() {\n\n")
+  file.write("\toverride fun <R> accept(visitor: Visitor<R>) : R {\n")
+  file.write("\t\treturn visitor.visit#{className}#{$basename}(this)\n")
+  file.write("\t}\n}\n\n")
+end
+
+def defineVisitor(file, types)
+  file.write("\tinterface Visitor<R> {\n")
+  types.each do |type|
+    info = type.split(';')
+    typename = info[0].strip
+    file.write("\t\tfun visit#{typename}#{$basename}(#{$basename.downcase}: #{typename}) : R\n")
+  end
+  file.write("\t}\n\n")
 end
 
 astDefinitions = [
@@ -14,7 +27,10 @@ astDefinitions = [
 
 path = File.expand_path("../../src/main/kotlin/Expr.kt")
 File::open(path, mode='w') do |file|
-  file.write("abstract class #{$basename}() {\n}\n\n")
+  file.write("abstract class #{$basename}() {\n\n")
+  defineVisitor(file, astDefinitions)
+  file.write("\tabstract fun <R> accept(visitor: Visitor<R>): R\n")
+  file.write("}\n\n")
 
   astDefinitions.each do |n|
     definitions = n.split(';')
@@ -22,5 +38,8 @@ File::open(path, mode='w') do |file|
     fields = definitions[1].strip
     defineType(file, className, fields)
   end
+
+
+
 
 end
--- src/main/kotlin/Expr.kt
+++ src/main/kotlin/Expr.kt
@@ -1,15 +1,40 @@
 abstract class Expr() {
+
+	interface Visitor<R> {
+		fun visitBinaryExpr(expr: Binary) : R
+		fun visitGroupingExpr(expr: Grouping) : R
+		fun visitLiteralExpr(expr: Literal) : R
+		fun visitUnaryExpr(expr: Unary) : R
+	}
+
+	abstract fun <R> accept(visitor: Visitor<R>): R
 }
 
 class Binary(left: Expr, operator: Token, right: Expr) : Expr() {
+
+	override fun <R> accept(visitor: Visitor<R>) : R {
+		return visitor.visitBinaryExpr(this)
+	}
 }
 
 class Grouping(expression: Expr) : Expr() {
+
+	override fun <R> accept(visitor: Visitor<R>) : R {
+		return visitor.visitGroupingExpr(this)
+	}
 }
 
 class Literal(value: Any?) : Expr() {
+
+	override fun <R> accept(visitor: Visitor<R>) : R {
+		return visitor.visitLiteralExpr(this)
+	}
 }
 
 class Unary(operator: Token, right: Expr) : Expr() {
+
+	override fun <R> accept(visitor: Visitor<R>) : R {
+		return visitor.visitUnaryExpr(this)
+	}
 }
 
